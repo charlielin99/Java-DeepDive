853fa0ba8f7f14b420d4a4481b48beb94dc44391
Charlie Lin <hlin266@uwo.ca>
2018-06-20 00:17:33 -0400

    Finished SinglyLinkedList

---
+++ Lists/SinglyLinkedList/src/com/company/EmployeeNode.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class EmployeeNode {
+
+    private Employee employee;
+    private EmployeeNode next; //next node in list
+
+    public EmployeeNode(Employee employee){
+        this.employee = employee;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public EmployeeNode getNext() {
+        return next;
+    }
+
+    public void setNext(EmployeeNode next) {
+        this.next = next;
+    }
+
+    public String toString(){
+        return employee.toString();
+    }
+
+}
+
--- Lists/SinglyLinkedList/src/com/company/Main.java
+++ Lists/SinglyLinkedList/src/com/company/Main.java
@@ -1,11 +1,31 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
-	// why implement this when there is a JDK built in??
+
+        Employee janeJones = new Employee("jane", "jones", 123);
+        Employee johndoe = new Employee("john", "doe", 123);
+        Employee marysmith = new Employee("mary", "smith", 123);
+        Employee mikewilson = new Employee("mike", "wilson", 123);
+
+        EmployeeLinkedList list = new EmployeeLinkedList();
+
+        System.out.println(list.getSize());
+
+
+        list.addToFront(janeJones);
+        list.addToFront(johndoe);
+        list.addToFront(marysmith);
+        list.addToFront(mikewilson);
+
+        list.removeFromFront();
+
+        list.printList();
+
+
+
+
     }
 }

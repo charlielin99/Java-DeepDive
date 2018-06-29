f0f59071703e5268a39e6e57532766c65c0320a7
Charlie Lin <hlin266@uwo.ca>
2018-06-29 00:00:06 -0400

    Finish CircularQueue

---
+++ Queues/CircularQueue/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee janeJones = new Employee("jane", "jones", 123);
+        Employee johndoe = new Employee("john", "doe", 123);
+        Employee marysmith = new Employee("mary", "smith", 123);
+        Employee mikewilson = new Employee("mike", "wilson", 123);
+
+
+    }
+}

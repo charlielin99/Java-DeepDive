b3e33152dcea69afc5dea34c420b00c7cd3ef409
Charlie Lin <hlin266@uwo.ca>
2018-06-30 20:38:04 -0400

    Finished Queue Challenge

--- Queues/QueueChallenge/src/com/company/Main.java
+++ Queues/QueueChallenge/src/com/company/Main.java
@@ -19,7 +19,25 @@ public class Main {
 
     public static boolean checkForPalindrome(String string) {
 
-        return false;
+        LinkedList<Character> stack = new LinkedList<Character>();
+        LinkedList<Character> queue = new LinkedList<Character>();
+        String lowerCase = string.toLowerCase();
+
+        for (int i=0;i<lowerCase.length();i++){
+            char c = lowerCase.charAt(i);
+            if (c >= 'a' && c <= 'z'){
+                queue.addLast(c);
+                stack.push(c);
+            }
+        }
+
+        while(!stack.isEmpty()){
+            if (!stack.pop().equals(queue.removeFirst())){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
 

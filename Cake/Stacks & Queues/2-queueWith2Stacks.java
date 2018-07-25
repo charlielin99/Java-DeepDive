9aa4da4b54e5a8cd574efeee9c1d285acd9e8d4b
Charlie Lin <hlin266@uwo.ca>
2018-07-25 00:01:38 -0400

    Queues & Stacks Mastery

---
+++ Cake/Stacks & Queues/2-queueWith2Stacks.java
@@ -0,0 +1,98 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.Assert.*;
+
+public class Solution {
+
+    // fill in the definitions for enqueue() and dequeue()
+    
+
+    public static class QueueTwoStacks {
+
+        private Deque<Integer> inStack = new ArrayDeque<>();
+        private Deque<Integer> outStack = new ArrayDeque<>();
+        
+        public void enqueue(int item) {
+            inStack.push(item);
+        }
+
+        public int dequeue() {
+            if (outStack.isEmpty()){
+                while (!inStack.isEmpty()){
+                    outStack.push(inStack.pop());
+                }
+                
+                if (outStack.isEmpty()){
+                    throw new IllegalArgumentException("Cannot Dequeue empty");
+                }
+            }
+            
+            return outStack.pop();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // tests
+
+    @Test
+    public void basicQueueOperationsTest() {
+        final QueueTwoStacks q = new QueueTwoStacks();
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        assertEquals("dequeue #1", 1, q.dequeue());
+        assertEquals("dequeue #2", 2, q.dequeue());
+        q.enqueue(4);
+        assertEquals("dequeue #3", 3, q.dequeue());
+        assertEquals("dequeue #4", 4, q.dequeue());
+    }
+
+    @Test(expected = Exception.class)
+    public void exceptionWhenDequeueFromNewQueueTest() {
+        final QueueTwoStacks q = new QueueTwoStacks();
+        q.dequeue();
+    }
+
+    @Test(expected = Exception.class)
+    public void exceptionWhenDequeueFromEmptyQueueTest() {
+        final QueueTwoStacks q = new QueueTwoStacks();
+        q.enqueue(1);
+        q.enqueue(2);
+        q.dequeue();
+        q.dequeue();
+        q.dequeue();
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+
+}
